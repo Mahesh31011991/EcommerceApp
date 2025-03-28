300fc6254f943be32d87bab1d7689fd323fadc5b
Apple <apple@Apples-MacBook-Pro.local>
2025-03-28 16:11:54 +0530

    Inital commit where .gitignore file has been added in Local Repo

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Apple on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
