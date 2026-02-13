64e0f09272567d7696b6f5c8d9db586784ded481
Mahesh Behere <maheshbehere14@gmail.com>
2026-02-13 22:04:18 +0530

    Added a Login Successfully.
    Login successfully and redirect to the Dashboard screen.

--- EcommerceApp/VIew/AuthView.swift
+++ EcommerceApp/VIew/AuthView.swift
@@ -13,46 +13,55 @@ struct AuthView: View {
     @State private var password = ""
     @State private var isLoginMode = true
     @State private var errorMessage = ""
+    @State private var navigateToDashboard = false
     
     var body: some View {
-        
-        VStack{
-            Text(isLoginMode ? "Login" : "SignUp")
-                .font(.largeTitle)
-                .bold()
-                .padding()
+        NavigationView {
             
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            Button(action: handleAuth) {
-                Text(isLoginMode ? "Login" : "SignUP")
-                    .foregroundColor(.white)
+            VStack{
+                Text(isLoginMode ? "Login" : "SignUp")
+                    .font(.largeTitle)
+                    .bold()
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+                
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-            }
-            
-            Text(errorMessage)
-                .foregroundColor(.red)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: handleAuth) {
+                    Text(isLoginMode ? "Login" : "SignUP")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+                
+                Button {
+                    isLoginMode.toggle()
+                } label: {
+                    Text(isLoginMode ? "Don't have an account? Sign Up" : "Already have an account?")
+                        .foregroundColor(.blue)
+                }
                 .padding()
-            
-            Button {
-                isLoginMode.toggle()
-            } label: {
-                Text(isLoginMode ? "Don't have an account? Sign Up" : "Already have an account?")
-                    .foregroundColor(.blue)
+                
+                NavigationLink(
+                    destination: DashBoardView(),
+                    isActive: $navigateToDashboard) {
+                        EmptyView()
+                    }
             }
             .padding()
         }
-        .padding()
     }
     
     func handleAuth(){
@@ -64,7 +73,9 @@ struct AuthView: View {
     }
     
     func loginUser(){
-        
+        if email == "Mahesh" && password == "12345"{
+            navigateToDashboard = true
+        }
     }
     
     func signUpUser(){
